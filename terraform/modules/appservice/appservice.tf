f0c3be1a0c52607d4f82357f905a5b0fd9c49245
eithar <e.abdullah.re@gmail.com>
2026-08-29 02:29:58 +0300

    Switch App Service to Windows to support classic ASP.NET FakeRestAPI package

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -2,27 +2,23 @@ resource "azurerm_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  os_type             = "Linux"
+  os_type             = "Windows"
   sku_name            = "F1"
-
   tags = {
     project = "cd1807-quality-releases"
   }
 }
-
-resource "azurerm_linux_web_app" "test" {
+resource "azurerm_windows_web_app" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   service_plan_id     = azurerm_service_plan.test.id
-
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = 0
   }
   site_config {
     always_on = false
   }
-
   tags = {
     project = "cd1807-quality-releases"
   }
